7804810cd662812cf858d1df3af2aff7118eebd5
Reloin <yihengleong@gmail.com>
2021-08-29 10:12:42 +0800

    add line s.t no further edit needed

--- swap.py
+++ swap.py
@@ -7,6 +7,7 @@ with open("master.json") as f:
     data = json.load(f)
     for key, value in data.items():
         angel_file.write("\t\"{}\":\"{}\",\n".format(value, key))
+angel_file.write("\t\"eof\":\"eof\"\n")
 angel_file.write("}")
 
 angel_file.close()
